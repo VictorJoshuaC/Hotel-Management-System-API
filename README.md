d29568fa6d3732dd25cbb3f0e82321b3bb34b422
victor joshua <vickyjoshua49@gmail.com>
2023-02-12 22:23:50 +0100

    arara

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
 # Hotel-Management-System-API
-Hotel management system API in Vanilla javascript with Node.js and Express.js
+
+Hotel Management API
+This is a Node.js and Express.js based API for hotel management. It provides endpoints for creating, retrieving, updating, and deleting room types and rooms.
+And also Node.js web server application that uses Express and MongoDB to provide APIs for a hotel management system. The application provides endpoints for storing and retrieving information about hotel rooms and room types.
+
+Dependencies
+express
+mongoose
+Connecting to the database
+The database connection string is hardcoded in the code and is using MongoDB Atlas:
+
+const mongoDB = "mongodb+srv://username1:nYzSBYeX9fmd8hMU@cluster2.aepci1g.mongodb.net/hotel-database?retryWrites=true&w=majority";
+Room Type Endpoints
+POST /api/v1/rooms-types: Store a new room type in the database
+GET /api/v1/rooms-types: Retrieve all room types from the database
+Room Endpoints
+POST /api/v1/rooms: Store a new room in the database
+GET /api/v1/rooms: Retrieve all rooms from the database with filtering options available as query parameters
+search: A string used to search for rooms with matching names (case-insensitive)
+roomType: Filter rooms by room type id
+minPrice: Filter rooms with a minimum price
+maxPrice: Filter rooms with a maximum price
+PATCH /api/v1/rooms/:id: Update an existing room in the database using its id
+DELETE /api/v1/rooms/:id: Delete an existing room from the database using its id
+GET /api/v1/rooms/:id: Retrieve a specific room from the database using its id
+
+##Running the Application
+Clone the repository to your local machine
+Install the dependencies by running npm install in the terminal
+Start the application by running npm start in the terminal
+The server will start on port 3000, you can access the API endpoints using a web browser or a tool such as Postman
+
+##Note
+The mongoose.set('strictQuery',true) line has been added to enable strict query mode. This helps to catch query-related bugs and improve security by ensuring that only the defined fields can be queried.
--- main.js
+++ main.js
@@ -1,1 +1,115 @@
-console.log("we here");
+const express = require("express");
+const mongoose = require("mongoose");
+const app = express();
+
+// Connect to the database
+mongoose.set('strictQuery',true);
+const mongoDB = "mongodb+srv://username1:nYzSBYeX9fmd8hMU@cluster2.aepci1g.mongodb.net/hotel-database?retryWrites=true&w=majority";
+mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true }).then(() => console.log('connected')).catch(err => console.log(err))
+
+// Room Type schema
+const roomTypeSchema = new mongoose.Schema({
+  name: String
+});
+
+// Room schema
+const roomSchema = new mongoose.Schema({
+  name: String,
+  roomType: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "RoomType"
+  },
+  price: Number
+});
+
+// Room Type model
+const RoomType = mongoose.model("RoomType", roomTypeSchema);
+
+// Room model
+const Room = mongoose.model("Room", roomSchema);
+
+// POST endpoint for storage of room types
+app.post("/api/v1/rooms-types", (req, res) => {
+  const roomType = new RoomType({
+    name: req.body.name
+  });
+
+  roomType.save((err, roomType) => {
+    if (err) return res.status(500).send(err);
+    res.status(201).send(roomType);
+  });
+});
+
+// GET endpoint for fetching of all room types
+app.get("/api/v1/rooms-types", (req, res) => {
+  RoomType.find((err, roomTypes) => {
+    if (err) return res.status(500).send(err);
+    res.send(roomTypes);
+  });
+});
+
+// POST endpoint for storage of rooms
+app.post("/api/v1/rooms", (req, res) => {
+  const room = new Room({
+    name: req.body.name,
+    roomType: req.body.roomType,
+    price: req.body.price
+  });
+
+  room.save((err, room) => {
+    if (err) return res.status(500).send(err);
+    res.status(201).send(room);
+  });
+});
+
+
+
+
+
+// Room model
+// const Room = mongoose.model("Room", roomSchema);
+
+// GET endpoint for fetching all the rooms with filters
+app.get("/api/v1/rooms", (req, res) => {
+  const search = req.query.search;
+  const roomType = req.query.roomType;
+  const minPrice = req.query.minPrice || 0;
+  const maxPrice = req.query.maxPrice || Number.MAX_SAFE_INTEGER;
+
+  Room.find({
+    name: new RegExp(search, "i"),
+    roomType: roomType,
+    price: { $gte: minPrice, $lte: maxPrice }
+  }, (err, rooms) => {
+    if (err) return res.status(500).send(err);
+    res.send(rooms);
+  });
+});
+
+// PATCH endpoint for editing a room using its id
+app.patch("/api/v1/rooms/:id", (req, res) => {
+  Room.findByIdAndUpdate(req.params.id, req.body, { new: true }, (err, room) => {
+    if (err) return res.status(500).send(err);
+    res.send(room);
+  });
+});
+
+// DELETE endpoint for deleting a room using its id
+app.delete("/api/v1/rooms/:id", (req, res) => {
+  Room.findByIdAndRemove(req.params.id, (err, room) => {
+    if (err) return res.status(500).send(err);
+    res.send(room);
+  });
+});
+
+// GET endpoint for fetching a room using its id
+app.get("/api/v1/rooms/:id", (req, res) => {
+  Room.findById(req.params.id, (err, room) => {
+    if (err) return res.status(500).send(err);
+    res.send(room);
+  });
+});
+
+app.listen(3000, () => {
+  console.log("Server started on port 3000");
+});
